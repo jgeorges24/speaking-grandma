c82db8257dffefe9556fa8b8ad6e9e85884570df
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-04 10:59:14 -0500

    commit tree 40e56d3a446e2bb0b056e9c9ed3c2630b59aba90

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,17 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma (phase)
+    if phase == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif phase != phase.upcase
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif phase == "WHAT DID YOU EAT TODAY?"
+        return "NO, NOT SINCE 1938!"
+    elsif phase == "WHAT?"
+        return "NO, NOT SINCE 1938!"
+    end
+end
+
